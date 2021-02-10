a5c56fd9ab5f7ec18c97646cf27068fb702474ef
Félix Vicente <felixvicent1306@gmail.com>
2021-02-10 10:04:09 -0300

    Customize tab navigation

--- src/navigations/TabNavigation.js
+++ src/navigations/TabNavigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Chats from '../screens/Chats';
 import Users from '../screens/Users';
@@ -9,9 +10,34 @@ const Tabs = createBottomTabNavigator();
 
 const TabNavigation = () => (
   <NavigationContainer>
-    <Tabs.Navigator>
-      <Tabs.Screen name="Chats" component={Chats} />
-      <Tabs.Screen name="Users" component={Users} />
+    <Tabs.Navigator
+      tabBarOptions={{
+        activeBackgroundColor: '#fff',
+        inactiveBackgroundColor: '#fff',
+        activeTintColor: '#0085ff',
+        inactiveTintColor: '#85c3ff',
+        keyboardHidesTabBar: true,
+      }}>
+      <Tabs.Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="chat" size={28} color={color} />
+          ),
+          title: 'Conversas',
+        }}
+        name="Chats"
+        component={Chats}
+      />
+      <Tabs.Screen
+        options={{
+          title: 'Pessoas',
+          tabBarIcon: ({ color }) => (
+            <Icon name="person" size={28} color={color} />
+          ),
+        }}
+        name="Users"
+        component={Users}
+      />
     </Tabs.Navigator>
   </NavigationContainer>
 );
