7eb544c7552c5da89ad2cf580c01ffd1ea63702c
Félix Vicente <felixvicent1306@gmail.com>
2021-02-13 10:15:12 -0300

    Create register screen

--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,14 +1,45 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 
+import logo from '../../assets/logo.png';
+
+import Input from '../../components/Input';
 import styles from './styles';
 
-const Register = () => {
+const Login = ({ navigation }) => {
+  const handleRegister = () => {
+    navigation.replace('Login');
+  };
+
+  const handleLoginNavigation = () => {
+    navigation.navigate('Login');
+  };
+
   return (
-    <View>
-      <Text>Register</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image style={styles.logo} source={logo} />
+        <Text style={styles.title}>WhisperApp</Text>
+        <Text style={styles.subTitle}>O novo modo de fazer fofoca</Text>
+
+        <Text style={styles.descriptionTitle}>CRIAR SUA CONTA</Text>
+      </View>
+
+      <Input placeholder="Seu nome" />
+      <Input placeholder="Sua senha" secureTextEntry={true} />
+
+      <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
+        <Text style={styles.registerButtonText}>Registrar</Text>
+      </TouchableOpacity>
+
+      <View style={styles.loginContainer}>
+        <Text style={styles.loginLabel}>Já tem conta? </Text>
+        <TouchableOpacity onPress={handleLoginNavigation}>
+          <Text style={styles.loginLinkText}>Faça login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
-export default Register;
+export default Login;
--- src/screens/Register/styles.js
+++ src/screens/Register/styles.js
@@ -1,5 +1,58 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    justifyContent: 'center',
+  },
+  header: {
+    alignItems: 'center',
+  },
+  logo: {
+    width: 120,
+    height: 75,
+    margin: 24,
+  },
+  title: {
+    fontSize: 22,
+    color: '#aaa',
+    fontWeight: 'bold',
+  },
+  subTitle: {
+    fontSize: 14,
+    color: '#ccc',
+    fontWeight: 'bold',
+  },
+  descriptionTitle: {
+    color: '#0085ff',
+    marginTop: 50,
+  },
+  registerButton: {
+    marginTop: 20,
+    backgroundColor: '#0085ff',
+    borderRadius: 20,
+    height: 46,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  registerButtonText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  loginContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 8,
+  },
+  loginLabel: {
+    color: '#aaa',
+    marginRight: 2,
+  },
+  loginLinkText: {
+    color: '#0085ff',
+  },
+});
 
 export default styles;
